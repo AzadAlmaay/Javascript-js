6048d54883890352891fc8973e56bdfb10a28163
AzadAlmaay <Azadalmaay093@gmail.com>
2023-01-15 23:10:31 +0100

    made js first time

---
+++ code.js
@@ -0,0 +1,13 @@
+
+
+//let name = "Azad";
+//let age = 20;
+let name = 'Azad' , age = 28 ;
+let salary = 2000;
+
+if (age > 20)
+console.log('Enter');
+
+console.log (name);
+console.log (age);
+console.log (salary);
